c8de4261d834443886b94fb45a274f5e8ae72c70
cvcod <35714533+cvcod@users.noreply.github.com>
2018-03-20 14:45:51 -0400

    Add files via upload

---
+++ WindowsFormsApp1/frmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Direc = new System.Windows.Forms.TextBox();
+            this.Extension = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.TextBox();
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Direc
+            // 
+            this.Direc.Location = new System.Drawing.Point(146, 61);
+            this.Direc.Name = "Direc";
+            this.Direc.Size = new System.Drawing.Size(263, 20);
+            this.Direc.TabIndex = 0;
+            // 
+            // Extension
+            // 
+            this.Extension.Location = new System.Drawing.Point(146, 103);
+            this.Extension.Name = "Extension";
+            this.Extension.Size = new System.Drawing.Size(263, 20);
+            this.Extension.TabIndex = 1;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(146, 149);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(263, 20);
+            this.Search.TabIndex = 2;
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(21, 213);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(388, 173);
+            this.lbFiles.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search Directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Extension:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Text to Match:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(21, 417);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Search Files";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(334, 417);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 459);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbFiles);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Extension);
+            this.Controls.Add(this.Direc);
+            this.Name = "frmMain";
+            this.Text = "File Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Direc;
+        private System.Windows.Forms.TextBox Extension;
+        private System.Windows.Forms.TextBox Search;
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ WindowsFormsApp1/frmMain.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Direc.Text = String.Empty;
+                Extension.Text = String.Empty; ;
+                Search.Text = String.Empty;
+                lbFiles.Items.Clear();
+            }
+            catch(Exception ex)
+            {
+                Logger(ex.ToString(), DateTime.Now);
+            }
+
+        }
+
+
+
+            private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Logger("Search Started", DateTime.Now);
+
+                string path = Direc.Text; 
+                string ext = Extension.Text;
+                string searchtxt = Search.Text;
+
+                FindFiles(path, ext, searchtxt);
+                Subdirectories(path, ext, searchtxt);
+
+                Logger("Search Ended", DateTime.Now);
+
+            }
+            catch (Exception ex)
+            {
+                Logger(ex.ToString(), DateTime.Now);
+            }
+
+
+        }
+
+
+
+        private bool searchFile(string fileName, string searchString)
+        {
+            try
+            {
+                FileStream fstream = new FileStream(fileName, FileMode.Open);
+                StreamReader stread = new StreamReader(fstream);
+                string line;
+
+                line = stread.ReadLine();
+
+                while (line != null)
+                {
+                    if (line.ToLower().Contains(searchString.ToLower()))
+                    {
+                        stread.Close();
+                        return true;
+                    }
+
+                    line = stread.ReadLine();
+                }
+
+                stread.Close();
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+           
+        }
+
+
+
+
+        private void FindFiles(string path, string ext, string searchtxt)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(path, "*" + ext);
+
+                foreach (string file in files)
+                {
+                    if (searchFile(file, searchtxt))
+                    { lbFiles.Items.Add(file); }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+
+       private void Subdirectories(string path, string ext, string searchtxt)
+        {
+            try
+            {
+                string[] direcs = Directory.GetDirectories(path);
+
+                foreach (string dir in direcs)
+                {
+                    FindFiles(dir, ext, searchtxt);
+                    Subdirectories(dir, ext, searchtxt);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+       }
+
+
+
+
+        private void Logger(string proc, DateTime dt)
+        {
+            try
+            {
+                FileStream fs = new FileStream("../../log/log.txt", FileMode.Append);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine($"{dt}: {proc}");
+                sw.Close();
+            }
+            catch( Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        
+    }
+}
